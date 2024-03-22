b289900c669aeb5a6294ed5717fc0dd29929ae68
Georgi Santana <georgi.andersson@hotmail.com>
2024-03-22 10:33:39 -0300

    Aplicação do conceito Statefull para adicionar usuários. Cabeçalho de requisição e resposta, e definição de formato de dados para trafego na aplicação.

--- src/server.js
+++ src/server.js
@@ -1,7 +1,46 @@
 import http from 'node:http'
 
-const server = http.createServer((rec, res) => {
-   return res.end('Hello Word')
+// - Criar usuários
+// - Listagem usuários
+// - Edição de usuários
+// - Remoção de usuários
+
+// - HTTP
+//   - Método HTTP
+//   - URL
+
+// GET, POST, PUT, PATCH, DELETE
+
+// GET => Buscar um recurso do back-end
+// POST => Criar um recurso no back-end
+// PUT => Atualizar um recurso no back-end
+// PATCH => Atulaizar um informação especifica de um recurso no back-end
+// DELETE => Deletar um recurso do back-end
+
+// Aplicação STATEFUL - Salvam os dados localmente em memoria
+// Aplicação STATELESS - Salvam os dados em BD externos
+
+const users = []
+
+const server = http.createServer((req, res) => {
+   const {method, url} = req
+
+   if (method === "GET" && url === "/users") {
+      return res
+      .setHeader('Content-type', 'application/json')
+      .end(JSON.stringify(users))
+   }
+
+   if (method === "POST" && url === "/users") {
+      users.push({
+         id: 1,
+         name: "George Sant",
+         email: "georgesant@example.com",
+      })
+      return res.end("Criação de usuário")
+//    _res.end_ para retornar um texto        
+   }
+   return res.end("Hello Word")
 })
 
-
+server.listen(3333);
