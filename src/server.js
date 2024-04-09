85c781917c3974812934d6ac748f8a6e269fa266
Georgi Santana <georgi.andersson@hotmail.com>
2024-04-09 10:34:08 -0300

    Organizando o arquivo de routes, eliminando a utilização de vários if no arquivo server.

--- src/server.js
+++ src/server.js
@@ -1,59 +1,21 @@
 import http from 'node:http'
 import { json } from './middiewares/json.js'
-import { Database } from './database.js'
-
-// - Criar usuários
-// - Listagem usuários
-// - Edição de usuários
-// - Remoção de usuários
-
-// - HTTP
-//   - Método HTTP
-//   - URL
-
-// GET, POST, PUT, PATCH, DELETE
-
-// GET => Buscar um recurso do back-end
-// POST => Criar um recurso no back-end
-// PUT => Atualizar um recurso no back-end
-// PATCH => Atulaizar um informação especifica de um recurso no back-end
-// DELETE => Deletar um recurso do back-end
-
-// Aplicação STATEFUL - Salvam os dados localmente em memoria
-// Aplicação STATELESS - Salvam os dados em BD externos
-
-// Cabeçalhos (Requisições/respostas) => Metadados
-
-// HTTP Status Code
-
-const database = new Database()
+import { routes } from './middiewares/routes.js'
 
 const server = http.createServer(async (req, res) => {
    const {method, url} = req
 
    await json(req, res)
-   
-   if (method === "GET" && url === '/users') {
-      const users = database.select('users')
-      
-      return res.end(JSON.stringify(users))
+
+   const route = routes.find(route => {
+      return route.method === method && route.path === url
+   })
+
+   if (route) {
+      return route.handler(req, res)
    }
-
-   if (method === "POST" && url === "/users") {
-      const { name, email } = req.body
-
-     const user = ({
-         id: 1,
-         name,
-         email,
-      })
-
-      database.insert('users', user)
-
-      return res.writeHead(201).end()
-//    _res.end_ para retornar um texto        
-   }
-   return res.writeHead(404).end()
+          
+      return res.writeHead(404).end()
 })
 
 server.listen(3333);
