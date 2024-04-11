a58670a0fa29e23b84312c84aa1c59be0a63a1a5
Georgi Santana <georgi.andersson@hotmail.com>
2024-04-11 13:27:31 -0300

    Seguindo o desenvolvimento para acessar os parâmetros das rotas, foi criada  uma função que vai utilizar de uma RegEx para as futuras operações.

--- src/middiewares/routes.js
+++ src/middiewares/routes.js
@@ -1,15 +1,15 @@
 import { Database } from "../database.js"
 import { randomUUID } from 'node:crypto'
+import {  buildRoutePath } from "../utils/build-route-path.js"
 
 const database = new Database()
 
 export const routes = [
     {
         method: 'GET',
-        path: '/users',
+        path: buildRoutePath('/users'),
         handler: (req, res) => {
-
-            const users = database.select('users')
+          const users = database.select('users')
 
             return res.end(JSON.stringify(users))
         }
@@ -18,10 +18,9 @@ export const routes = [
 
     {
         method: 'POST',
-        path: '/users',
+        path: buildRoutePath('/users'),
         handler: (req, res) => {
-
-            const { name, email, } = req.body
+           const { name, email, } = req.body
 
             const user = {
                 id: randomUUID(),
@@ -33,5 +32,12 @@ export const routes = [
 
             return res.writeHead(201).end()
   }
- } 
+ },
+ {
+    method: 'DELETE',
+    path: buildRoutePath('/users/:id'),
+    handler: (req, res) => {
+        return res.end()
+    },
+ }
 ]
