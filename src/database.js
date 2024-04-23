529c7015d64452083dc2ca0a14d3556d9a0fde42
Georgi Santana <georgi.andersson@hotmail.com>
2024-04-23 20:13:41 -0300

    Realizando os acessos com  Query Params

--- src/database.js
+++ src/database.js
@@ -37,6 +37,15 @@ export class Database {
       return data;
     }
 
+    updata(table, id) {
+      const rowIndex = this.#database[table].findIndex(row => row.id === id)
+
+      if (rowIndex > -1) {
+        this.#database[table][rowIndex] = {id, ...data}
+        this.#persist()
+      }
+    }
+
     delete(table, id) {
       const rowIndex = this.#database[table].findIndex(row => row.id === id)
 
--- src/middiewares/routes.js
+++ src/middiewares/routes.js
@@ -9,6 +9,7 @@ export const routes = [
         method: 'GET',
         path: buildRoutePath('/users'),
         handler: (req, res) => {
+            console.log(req.query)
           const users = database.select('users')
 
             return res.end(JSON.stringify(users))
@@ -32,6 +33,19 @@ export const routes = [
 
             return res.writeHead(201).end()
   }
+ },
+
+ {
+    method: 'PUT',
+    path: buildRoutePath('/users/:id'),
+    handler: (req, res) => {
+        const { id } = req.params
+        const { name, email } = req.body
+
+        database.delete('users', id)
+
+        return res.writeHead(204).end()
+    },
  },
  {
     method: 'DELETE',
